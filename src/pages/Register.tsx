647e0bce2722a1e84562b321d642b707dc5c47e6
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-17 22:33:16 +0545

    register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,8 +4,8 @@ import Button from "../components/Button";
 
 const Register = () => {
   return (
-    <div className="w-full h-screen flex items-center bg-bgClr px-10">
-      <form className="rounded-[16px] bg-bgComp px-5 py-[40px] xl:py-[60px] w-[350px] xl:w-[450px] 2xl:w-[600px] mx-auto my-auto grid gap-10 2xl:gap-14">
+    <div className="w-full min-h-screen flex items-center bg-bgClr px-10">
+      <form className="rounded-[16px] bg-bgComp px-5 py-[20px] xl:py-[30px] w-[350px] xl:w-[450px] 2xl:w-[600px] mx-auto my-auto grid gap-10 2xl:gap-14">
         <div className="w-full flex flex-col gap-3 2xl:gap-5 justify-center text-center items-center">
           <img className="size-[80px]" src={Logo} alt="" />
           <div className="text-center">
@@ -14,6 +14,8 @@ const Register = () => {
         </div>
 
         <div className="grid gap-3">
+          <Input type="text" placeholder="Last name" />
+          <Input type="text" placeholder="First name" />
           <Input type="text" placeholder="Email" />
           <Input type="password" placeholder="Password" />
           <Input type="password" placeholder="Confirm assword" />
