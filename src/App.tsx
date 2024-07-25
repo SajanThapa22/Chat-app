3e4ddea52c369db55896fc7ec105844dedb46a6a
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-25 22:20:07 +0545

    search bar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import ThemeSwitch from "./components/ThemeSwitch";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ChatUI from "./pages/chatUI";
 
 const App = () => (
   <>
     {/* <ThemeSwitch /> */}
     {/* <Login /> */}
-    <Register />
+    {/* <Register /> */}
+    <ChatUI />
   </>
 );
 
--- src/pages/chatUI.tsx
+++ src/pages/chatUI.tsx
@@ -1,27 +1,26 @@
-import { useState } from "react";
-
-const chatUI = () => {
-  const [user, setuser] = useState();
+import { CiSearch } from "react-icons/ci";
+const ChatUI = () => {
   return (
-    <div className="grid grid-cols-[1]">
-      <div id="all-chats" className="bg-bgComp">
+    <div className="grid grid-cols-1 xl:grid-cols-[1fr,3fr]">
+      <div id="all-chats" className="bg-bgComp px-4">
         <div>
           <div>Chats</div>
 
-          <div className="rounded-[10px] border border-gray-400 px-4 py-2">
+          <div className="rounded-[8px] border border-gray-400 px-2 py-1 flex gap-2">
             <input
               type="text"
-              className="w-full focus:outline-none border-none"
+              className="w-full focus:outline-none border-none text-border-gray-400"
             />
+            <CiSearch className="size-6" />
           </div>
         </div>
 
         <div id="user-chats"></div>
       </div>
 
-      <div id="chat-section"></div>
+      <div id="chat-section">i am sajan</div>
     </div>
   );
 };
 
-export default chatUI;
+export default ChatUI;
