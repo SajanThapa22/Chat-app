6a133e3480b2be9231bb68102fbad4219cac3059
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-25 23:24:55 +0545

    user details

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,12 +1,17 @@
-const User = () => {
+interface Props {
+  img: string;
+  userName: string;
+}
+
+const User = ({ img }: Props) => {
   return (
     <div className="flex gap-3">
       <div className="rounded-full aspect-square overflow-hidden size-10">
-        <img src="" alt="" />
+        <img src={img} alt="" />
       </div>
 
       <div>
-        <div>user name</div>
+        <div>{userName}</div>
         <div className="flex gap-2">
           message...
           <div>4:50 pm</div>
--- src/pages/chatUI.tsx
+++ src/pages/chatUI.tsx
@@ -2,23 +2,28 @@ import { CiSearch } from "react-icons/ci";
 const ChatUI = () => {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-[1fr,3fr]">
-      <div id="all-chats" className="bg-bgComp px-4">
+      <div
+        id="all-chats"
+        className="bg-bgComp px-4 border-r border-r-[#d1d1d1] min-h-screen"
+      >
         <div>
           <div>Chats</div>
 
           <div className="rounded-[8px] border border-gray-400 px-2 py-1 flex gap-2">
             <input
               type="text"
-              className="w-full focus:outline-none border-none text-border-gray-400"
+              className="w-full focus:outline-none border-none "
             />
-            <CiSearch className="size-6" />
+            <CiSearch className="size-6 text-gray-500" />
           </div>
         </div>
 
         <div id="user-chats"></div>
       </div>
 
-      <div id="chat-section">i am sajan</div>
+      <div id="chat-section" className="px-5">
+        i am sajan
+      </div>
     </div>
   );
 };
