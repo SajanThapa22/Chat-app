c1333d256017df16747b589d0d0f6ba95ada3496
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-25 23:58:48 +0545

    message part

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const User = ({ img, userName, message, time, style }: Props) => {
   return (
-    <div className={`flex gap-3 px-2 py-2 items-center ${style}`}>
+    <div className={`flex gap-4 px-2 py-2 items-center ${style}`}>
       <div className="rounded-full aspect-square overflow-hidden size-10">
         <img src={img} className="size-full" />
       </div>
