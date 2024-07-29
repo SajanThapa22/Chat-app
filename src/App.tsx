7991e5618394c1436ef1117b84bfe0f1dbf4d671
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-29 23:15:37 +0545

    login authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,7 @@
-// interface FetchOptions extends RequestInit {
-//   headers?: Record<string, string>;
-// }
+import React from "react";
 
-// const getToken = (): string | null => {
-//   return localStorage.getItem("token");
-// };
+const App = () => {
+  return <div>welcome to my app</div>;
+};
 
-// const fetchWithAuth = async (
-//   url: string,
-//   options: FetchOptions = {}
-// ): Promise<Response> => {
-//   const token = getToken();
-//   const headers: Record<string, string> = {
-//     "Content-Type": "application/json",
-//     ...options.headers,
-//   };
-
-//   if (token) {
-//     headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   const response = await fetch(url, {
-//     ...options,
-//     headers,
-//   });
-
-//   return response;
-// };
-
-// interface ApiResponse {
-//   // Define the expected structure of the response data here
-//   // For example:
-//   // id: number;
-//   // name: string;
-// }
-
-// const fetchData = async (): Promise<void> => {
-//   try {
-//     const response = await fetchWithAuth(
-//       "http://your-backend-url/api/some-endpoint",
-//       {
-//         method: "GET",
-//       }
-//     );
-
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-
-//     const data: ApiResponse = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error("Error fetching data", error);
-//   }
-// };
-
-// fetchData();
+export default App;
