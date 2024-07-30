095336ee0b7e9c84f793ec0a88ed0d1320d2a52f
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-30 22:12:17 +0545

    adding chat users

--- src/pages/chatUI.tsx
+++ src/pages/chatUI.tsx
@@ -52,9 +52,6 @@ const ChatUI = () => {
           <div className="rounded-full px-4 py-2 bg-primary max-w-fit text-white">
             hello sajan how are you man?
           </div>
-          <div className="rounded-full px-4 py-2 bg-primary max-w-fit text-white">
-            hello sajan how are you man?
-          </div>
         </div>
       </div>
     </div>
